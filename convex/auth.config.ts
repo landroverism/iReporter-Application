189956cb6e27c9a93e3f243157c27b657cb4384a
landroverism <kemboiham3@gmail.com>
2025-06-27 13:07:09 +0300

    changed url

--- convex/auth.config.ts
+++ convex/auth.config.ts
@@ -1,7 +1,9 @@
 export default {
   providers: [
     {
-      domain: process.env.CONVEX_SITE_URL,
+      domain: process.env.NODE_ENV === "production" 
+        ? "https://ireporter-new.netlify.app" 
+        : process.env.CONVEX_SITE_URL,
       applicationID: "convex",
     },
   ],
